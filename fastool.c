01fb793504330ef2c45e173fd0c74f8b8fd2e75d
Francesco Strozzi <francesco.strozzi@gmail.com>
2012-04-03 12:39:01 +0200

    added --illumina-trinity option

--- fastool.c
+++ fastool.c
@@ -18,76 +18,78 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 KSEQ_INIT(gzFile, gzread)
 
-
-int print_fasta(char *append, char *s[]) {
-	if (append == NULL) {
+int print_seq(char *append, int to_fasta, char *s[], int ilmn_trinity) {
+	if (ilmn_trinity && (s[0][strlen(s[0])-2] != '/') && s[3] != NULL) {
+		printf(">%s/%c\n", s[0], s[3][0]);
+	}
+	else if (append == NULL) {
 		printf(">%s\n", s[0]);
 	}
 	else {
 		printf(">%s%s\n", s[0], append);		
 	}
 	printf("%s\n", s[1]);
+	if (!to_fasta && s[2] != NULL && !ilmn_trinity) {
+		printf("+\n%s\n",s[2]);
+	}
 	return 0;
 }
 
-int print_fastq(char *append, char *s[]) {
-	if (append == NULL) {
-		printf("@%s\n", s[0]);		
-	}
-	else {
-		printf("@%s%s\n", s[0], append);		
-	}
-	printf("%s\n", s[1]);
-	printf("+\n%s\n",s[2]);
-	return 0;
-}
 
-void print_seq(char *append, int to_fasta, char *s[3]) {
-	if (to_fasta || s[2] == NULL) print_fasta(append, s);
-	else print_fastq(append, s);
-}
-
-
-int process_input(FILE *stream, int rev_comp, char *string, int to_fa) {
+int process_input(FILE *stream, int rev_comp, char *string, int to_fa, int ilmn_trin) {
 
 
 	kseq_t *seq;
 	seq = kseq_init(stream);
 	if (rev_comp) {
-	while (kseq_read(seq) >= 0) {
-		char rev_seq[seq->seq.l];
-		char *sequence_to_print[3] = {[2] = NULL};
-		for(int i = 0; i < seq->seq.l; ++i) {
-			if (*(seq->seq.s + seq->seq.l-1 - i) == 'A') rev_seq[i] = 'T';
-			else if (*(seq->seq.s + seq->seq.l-1 - i) == 'C') rev_seq[i] = 'G';
-			else if (*(seq->seq.s + seq->seq.l-1 - i) == 'T') rev_seq[i] = 'A';
-			else if (*(seq->seq.s + seq->seq.l-1 - i) == 'G') rev_seq[i] = 'C';
-			else if (*(seq->seq.s + seq->seq.l-1 - i) == 'N') rev_seq[i] = 'N';
-			else if (*(seq->seq.s + seq->seq.l-1 - i) == 'U') rev_seq[i] = 'A';
-		}
-		rev_seq[seq->seq.l] = '\0';
-		sequence_to_print[0] = seq->name.s;
-		sequence_to_print[1] = rev_seq;
-		
-		char quality[seq->qual.l];
-		if (seq->qual.l) {
-			for (int i = 0; i < seq->qual.l; ++i)
-			{
-				quality[i] = *(seq->qual.s + seq->qual.l -1 -i);
+		while (kseq_read(seq) >= 0) {
+			char rev_seq[seq->seq.l];
+			char *sequence_to_print[4] = {[2] = NULL};
+			for(int i = 0; i < seq->seq.l; ++i) {
+				if (*(seq->seq.s + seq->seq.l-1 - i) == 'A') rev_seq[i] = 'T';
+				else if (*(seq->seq.s + seq->seq.l-1 - i) == 'C') rev_seq[i] = 'G';
+				else if (*(seq->seq.s + seq->seq.l-1 - i) == 'T') rev_seq[i] = 'A';
+				else if (*(seq->seq.s + seq->seq.l-1 - i) == 'G') rev_seq[i] = 'C';
+				else if (*(seq->seq.s + seq->seq.l-1 - i) == 'N') rev_seq[i] = 'N';
+				else if (*(seq->seq.s + seq->seq.l-1 - i) == 'U') rev_seq[i] = 'A';
 			}
-			quality[seq->qual.l] = '\0';
-			sequence_to_print[2] = quality;
-		}
-		print_seq(string, to_fa, sequence_to_print);
-		}
+			rev_seq[seq->seq.l] = '\0';
+			sequence_to_print[0] = seq->name.s;
+			sequence_to_print[1] = rev_seq;
+			
+			
+			char quality[seq->qual.l];
+			if (seq->qual.s) {
+				for (int i = 0; i < seq->qual.l; ++i)
+				{
+					quality[i] = *(seq->qual.s + seq->qual.l -1 -i);
+				}
+				quality[seq->qual.l] = '\0';
+				sequence_to_print[2] = quality;
+			}
+
+			if (seq->comment.s) {
+				sequence_to_print[3] = seq->comment.s;
+			}
+			else {
+				sequence_to_print[3] = NULL;
+			}
+			print_seq(string, to_fa, sequence_to_print, ilmn_trin);
+			}
 	}
 	else {
 		while (kseq_read(seq) >= 0) {
-			char *sequence_to_print[3];
+			char *sequence_to_print[4];
 			sequence_to_print[0] = seq->name.s;
 			sequence_to_print[1] = seq->seq.s;
-			sequence_to_print[2] = seq->qual.s;
-			print_seq(string, to_fa, sequence_to_print);
+			if (seq->qual.s) sequence_to_print[2] = seq->qual.s;
+			if (seq->comment.s) {
+				sequence_to_print[3] = seq->comment.s;
+			}
+			else {
+				sequence_to_print[3] = NULL;
+			}
+			print_seq(string, to_fa, sequence_to_print, ilmn_trin);
 		}
 	}
 
@@ -96,7 +98,7 @@ int process_input(FILE *stream, int rev_comp, char *string, int to_fa) {
 }
 
 void print_help(char *command_line) {
-		printf("Usage: %s (--rev) (--append [string_to_append_to_header]) (--to-fasta) sequences_1.fastq/a sequences_2.fastq/a ... \n",command_line);
+		printf("Usage: %s (--rev) (--append [string_to_append_to_header]) (--to-fasta) (--illumina-trinity) sequences_1.fastq/a sequences_2.fastq/a ... \n",command_line);
 }
 
 int main(int argc, char *argv[])
@@ -106,6 +108,7 @@ int main(int argc, char *argv[])
 	char *string_to_append = NULL;
 	int to_fasta = 0;
 	int read_from_file = 0;
+	int illumina_trinity = 0;
 
 	if(argc == 1) {
 		print_help(argv[0]);
@@ -130,22 +133,27 @@ int main(int argc, char *argv[])
 				i++;
 			}
 		}
+		else if((strcmp(argv[i],"--illumina-trinity") == 0)) illumina_trinity = 1;
 		else {
-				read_from_file = 1;
-				gzFile fp;
-				if (!(fp = gzopen(argv[i],"r"))) {
-					printf("No %s file found!\n", argv[i]);
-					exit(0);
-				}
-				process_input(fp,reverse_complement, string_to_append, to_fasta);
-				gzclose(fp);
+			if (illumina_trinity && string_to_append) {
+				printf("You are using both --append and --illumina-trinity options. You can only provide one or the other.\n");
+				exit(0);
+			}
+			read_from_file = 1;
+			gzFile fp;
+			if (!(fp = gzopen(argv[i],"r"))) {
+				printf("No %s file found!\n", argv[i]);
+				exit(0);
+			}
+			process_input(fp,reverse_complement, string_to_append, to_fasta, illumina_trinity);
+			gzclose(fp);
 		}
 	}
 	if (!read_from_file)
 	{
 		gzFile fp_stdin;
 		fp_stdin = gzdopen(fileno(stdin), "rb");
-		process_input(fp_stdin, reverse_complement, string_to_append, to_fasta);
+		process_input(fp_stdin, reverse_complement, string_to_append, to_fasta, illumina_trinity);
 		gzclose(fp_stdin);
 	}
 
